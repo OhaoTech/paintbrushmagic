4733147b6e8c0f4e1d3df053dfbdfc649fb4b920
Frank Yin <djmax96945147@outlook.com>
2024-02-29 03:34:53 +0100

    (cont.) missing files uploaded

---
+++ gen/dependency.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+root_dir=$(pwd)
+cd GL
+npm install express
+
+cd -
+pip3 install -r requirements.txt
---
+++ gen/kill.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pkill -f script.py
+pkill -f server.py
+pkill -f server.js
+echo "All processes killed"
