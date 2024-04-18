89caddc1d09b57bc27896c5956463c8b81deec5a
FrankYin <djmax96945147@outlook.com>
2024-04-18 13:51:30 +0000

    [Panic fix] firewall unsafe behavior revoked. it will block other traffics unexpectedly eg. ssh:22. All firewall policies configured on Google consoles

--- gen/dependency.sh
+++ gen/dependency.sh
@@ -11,9 +11,3 @@ pip3 install -r requirements.txt
 echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 echo '~/.local/bin ADDED TO YOUR EXECUTABLE PATH'
-sudo iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 5500 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 7860 -j ACCEPT
-sudo ufw allow 7860/tcp
-sudo ufw allow 5500/tcp
-sudo ufw allow 5000/tcp
